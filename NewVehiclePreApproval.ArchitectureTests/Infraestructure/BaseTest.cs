688b096a062e0cd30563e640442bd978d4918bd0
Hector Caballero <caballero.22.809@gmail.com>
2025-02-10 13:26:52 -0600

    Add architecture tests and update Program.cs
    Updated Program.cs to include a partial class declaration and added a custom exception handler. Added a new project, NewVehiclePreApproval.ArchitectureTests, to the solution, targeting .NET 9.0 with necessary package references. Created BaseTest and LayerTests classes to enforce architectural boundaries between application layers.

---
+++ NewVehiclePreApproval.ArchitectureTests/Infraestructure/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿
+using NewVehiclePreApproval.Application.Abstractions.Messaging;
+using NewVehiclePreApproval.Domain.Abstractions;
+using NewVehiclePreApproval.Infrastructure;
+using System.Reflection;
+
+namespace NewVehiclePreApproval.ArchitectureTests.Infraestructure;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(Entity).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(IBaseCommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
